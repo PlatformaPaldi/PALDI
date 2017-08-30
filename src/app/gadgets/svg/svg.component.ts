6e689dcae43f22eecc48c052a584fadafb59bcc6
Administrador <hitalo.emanoel@gmail.com>
2017-08-30 10:48:55 -0300

    adicionado 2 imagens da história; adicionado o placeholder para as imagens

--- src/app/gadgets/svg/svg.component.ts
+++ src/app/gadgets/svg/svg.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Renderer2 } from '@angular/core';
 import { Gadget } from 'app/core/gadget';
+import { Svg } from 'app/core/svg';
 import { SectionService } from "app/core/section.service";
 
 @Component({
@@ -8,11 +9,16 @@ import { SectionService } from "app/core/section.service";
   styleUrls: ['./svg.component.css']
 })
 export class SvgComponent implements OnInit {
-  @Input() gadget: Gadget;
+  @Input() gadget: Svg;
+  img: any = 'assets/server/plholder.svg';
 
   constructor(private renderer: Renderer2, private sectionServ: SectionService) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.img = this.gadget.file;
   }
 
   prepare(svgElm: SVGElement) {
@@ -22,7 +28,7 @@ export class SvgComponent implements OnInit {
       this.renderer.listen(touchable, 'click', (evt) => {
         const state = this.sectionServ.currentState;
         console.log(state.behavior.onTouch);
-        
+
         state.behavior.onTouch(touchable.id);
       })
     }
